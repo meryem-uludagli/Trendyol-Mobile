ea63fc1b9c5fabb368362eeb84e86ac5d4394e7b
meryem-uludagli <meryemuslu2035@gmail.com>
2025-03-09 20:01:55 +0200

    add

--- src/router/rootNavigator.tsx
+++ src/router/rootNavigator.tsx
@@ -1,3 +1,4 @@
+import React, {useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import TabNavigator from './tabNavigator';
 import {AUTHNAVIGATOR, PRODUCTSNAVIGATOR, TABNAVIGATOR} from '../utils/routes';
@@ -5,19 +6,37 @@ import ProductList from '../screens/products';
 import ProductDetail from '../screens/products/productDetail';
 import {Colors} from '../theme/colors';
 import Login from '../screens/auth/login';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useDispatch} from 'react-redux';
+import {AppDispatch} from '../store';
+import {checkUser} from '../store/slice/authSlice';
 const Stack = createNativeStackNavigator();
-
 const RootNavigator: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const getState = async () => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      dispatch(checkUser(token));
+    }
+  };
+
+  useEffect(() => {
+    getState();
+  }, []);
+
   return (
     <Stack.Navigator
-      screenOptions={{headerBackTitle: 'Geri', headerTintColor: Colors.BLACK}}>
+      screenOptions={{
+        headerBackTitle: 'Geri',
+        headerTintColor: Colors.BLACK,
+      }}>
       <Stack.Screen
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+        }}
         name={TABNAVIGATOR.TABNAVIGATOR}
         component={TabNavigator}
       />
-
       <Stack.Screen
         name={PRODUCTSNAVIGATOR.PRODUCTSLIST}
         component={ProductList}
@@ -26,10 +45,8 @@ const RootNavigator: React.FC = () => {
         name={PRODUCTSNAVIGATOR.PRODUCTDETAIL}
         component={ProductDetail}
       />
-
       <Stack.Screen name={AUTHNAVIGATOR.LOGIN} component={Login} />
     </Stack.Navigator>
   );
 };
-
 export default RootNavigator;
